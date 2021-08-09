43f29d7f200dced1b0644696b7320586e63eccd7
hamzeh <hamza.abuleil@mail.huji.ac.il>
2021-08-09 17:39:55 +0300

    read_brain implemented

--- PartialRunValidator.py
+++ PartialRunValidator.py
@@ -3,36 +3,58 @@ import csv
 import sys
  # "ME_Ex/partialRun/partialRun_API.h"
 
-MONO = "ME_Ex/out/EyeQ4sw_rel/projects/EyeQ/Mono/brain/day_gsf_app.tmp.c"
-FOV = "ME_Ex/out/EyeQ4sw_rel/projects/EyeQ/2FOV/day_gsf_app.tmp.c"
-WONO = "ME_Ex/out/EyeQ4sw_rel/projects/EyeQ/Wono/brain/day_gsf_app.tmp.c"
+MONO_PATH = "out/EyeQ4sw_rel/projects/EyeQ/Mono/brain/day_gsf_app.tmp.c"
+FOV_PATH = "out/EyeQ4sw_rel/projects/EyeQ/2FOV/day_gsf_app.tmp.c"
+WONO_PATH = "out/EyeQ4sw_rel/projects/EyeQ/Wono/brain/day_gsf_app.tmp.c"
+
+TECHS_PATH = "partialRun/python/pr_techs_list.json"
+
+MONO = "Mono"
+FOV = "2FOV"
+WONO = "Wono"
 
 WRITE = 'w'
 READ = 'r'
 
 
-def write_data():
+def write_data(data):
     header = ["SEP", "Brain Type", "Partial Run Supported", "Partial Run Technology Type",
               "Partial Run Technology Type ID"]
-    data = [["S1", "MONO", "True", "UNKNOWN", "21"], ["S3", "FOV", "False", "UNKNOWN", "11"]]
+    # data = [["S1", "MONO", "True", "UNKNOWN", "21"], ["S3", "FOV", "False", "UNKNOWN", "11"]]
     with open("SEPs.csv", WRITE, newline='') as file:
         writer = csv.writer(file)
         writer.writerow(header)
         writer.writerows(data)
 
 
-def read_brain(brainType):
-    with open(brainType, READ) as file:
-        lines = [line.rstrip() for line in file]
-        return [line for line in lines if line]
+def read_brain(brain_path, brain_type):
+    data = []
+    techs = []
+    with open(TECHS_PATH, READ) as file:
+        techs = json.load(file)["pr_techs"]
+
+    with open(brain_path, READ) as file:
+        for line in file:
+            edited_line = line.rstrip()
+            if edited_line and "void SEP" in edited_line:
+                starting_index = edited_line.index("_") + 1
+                ending_index = edited_line.index("(")
+                sep_name = edited_line[starting_index:ending_index]
+                tech = "UNKNOWN"
+                for technology in techs:
+                    if technology in sep_name:
+                        tech = technology
+                        break
+                data.append([sep_name, brain_type, "UNKNOWN", tech, "UNKNOWN"])
+    return data
+
     # RETURN_IF_TECH_DISABLED_BY_PARTIAL_RUN(PartialRun::PRTechType::<TECH_NAME>)
     # PartialRun_API::isTechDisabledByPartialRun(techType)
 
 
 if __name__ == '__main__':
-    MONO_data = read_brain(MONO)
-    FOV_data = read_brain(FOV)
-    WONO_data = read_brain(WONO)
-    print(MONO_data[0:30])
+    MONO_data = read_brain(MONO_PATH, MONO)
+    FOV_data = read_brain(FOV_PATH, FOV)
+    WONO_data = read_brain(WONO_PATH, WONO)
     args = sys.argv[1:]
-    write_data()
+    write_data(MONO_data + FOV_data + WONO_data)
--- README.md
+++ README.md
@@ -4,3 +4,26 @@
 #complexity and space considerations
 #testing considerations
 #challenges/issues/possible solutions/other possible designs pros and cons
+
+
+#1) Paths:
+#a. MONO_PATH: "out/EyeQ4sw_rel/projects/EyeQ/Mono/brain/day_gsf_app.tmp.c"
+#b. 2FOV_PATH: "out/EyeQ4sw_rel/projects/EyeQ/2FOV/day_gsf_app.tmp.c"
+#b. 3FOV_PATH: "out/EyeQ4sw_rel/projects/EyeQ/3FOV"
+#c. WONO_PATH: "out/EyeQ4sw_rel/projects/EyeQ/Wono/brain/day_gsf_app.tmp.c"
+
+#2) instances of 'Partial Run Disabled' indicators and 'TECH_TYPES':
+#a. 
+    type: OnlineCalibration 
+    indicator: if (!PartialRun_API::isTechDisabledByPartialRun(PartialRun::PRTechType::OnlineCalibration))
+#b. 
+    type: WMBC
+    indicator: RETURN_IF_TECH_DISABLED_BY_PARTIAL_RUN(PartialRun::PRTechType::WMBC)
+#c.
+    type: TSR
+    indicator: if (!PartialRun_API::isTechDisabledByPartialRun(PartialRun::PRTechType::TSR))
+#d.
+    type: xMotion
+    indicator: RETURN_IF_TECH_DISABLED_BY_PARTIAL_RUN(PartialRun::PRTechType::xMotion)
+
+#3)
